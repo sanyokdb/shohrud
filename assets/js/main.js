b9fce504ff758db5eb45d11a4284dcf4c9849a9b
Sanyok <33344827+sanyokdb@users.noreply.github.com>
2024-10-13 01:23:27 +0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+// Бургер
+const burger = document.getElementById("burger");
+const nav = document.getElementById("nav");
+
+burger.addEventListener("click", function(){
+  nav.classList.toggle("is-active");
+  burger.classList.toggle("is-active");
+  document.body.classList.toggle("no-scroll");
+});
+
+
+// Swiper
+const HeroSwiper = new Swiper('.hero-slider', {
+  loop: true,
+  effect: "fade",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
+});
