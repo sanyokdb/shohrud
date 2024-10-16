4825605d9177167c2c268a2fd33a7fd28eb9543b
Sanyok <33344827+sanyokdb@users.noreply.github.com>
2024-10-16 13:01:23 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,3 +76,22 @@ function init() {
 }
 
 ymaps.ready(init);
+
+
+// scrollUp
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+    $("#scrollUp").fadeIn();
+  } else {
+    $("#scrollUp").fadeOut();
+  }
+});
+$("#scrollUp").click(function () {
+  $("body,html").animate(
+    {
+      scrollTop: 0,
+    },
+    500
+  );
+  return false;
+});
